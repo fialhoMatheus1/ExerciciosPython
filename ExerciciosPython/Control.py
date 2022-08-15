293c6361adf7cb8e5ea30b7be4968fdbdf28cfb0
fialhoMatheus1 <104318023+fialhoMatheus1@users.noreply.github.com>
2022-08-15 17:23:03 -0300

    Add files via upload

--- ExerciciosPython/Control.py
+++ ExerciciosPython/Control.py
@@ -11,6 +11,7 @@ class Control:
         self.idade = 0
         self.meses = 0
         self.dias = 0
+        self.macas = 0
 
     def getC(self):
         return self.c
@@ -48,6 +49,12 @@ class Control:
     def setDias(self, dias):
         self.dias = dias
 
+    def getMacas(self):
+        return self.macas
+
+    def setMacas(self, macas):
+        self.macas = macas
+
     def menu(self):
         print("")
         print("Escolha uma das opções abaixo:\n\n" +
@@ -60,6 +67,17 @@ class Control:
             "7. Exercício 7\n" +
             "8. Exercício 8\n" +
             "9. Exercício 9\n" +
+            "10. Exercício 10\n" +
+            "11. Exercício 11\n" +
+            "12. Exercício 12\n" +
+            "13. Exercício 13\n" +
+            "14. Exercício 14\n" +
+            "15. Exercício 15\n" +
+            "16. Exercício 16\n" +
+            "17. Exercício 17\n" +
+            "18. Exercício 18\n" +
+            "19. Exercício 19\n" +
+            "20. Exercício 20\n" +
             "0. Sair")
         self.opcao = int(input())
 
@@ -69,22 +87,23 @@ class Control:
             if self.opcao == 0:
                 print("Obrigado!!")
             elif self.opcao == 1:
-                print("EXERCÍCIO 1:")
-                print("A: 10\n"
-                      "B: 20")
+                print("EXERCÍCIO 1")
+                print("A: 10\n""B: 20")
                 print(self.model.exercicio01())
             elif self.opcao == 2:
-                print("EXERCÍCIO 2:")
+                print("EXERCÍCIO 2")
                 print("Digite um número: ")
                 self.setC(int(input()))
                 print("O antecessor é: {}".format(self.model.exercicio02(self.getC())))
             elif self.opcao == 3:
+                print("EXERCÍCIO 3")
                 print("Digite a base:")
                 self.setBase(int(input()))
                 print("Digite a altura:")
                 self.setAltura(int(input()))
                 print("A área é: {}".format(self.model.exercicio03(self.getBase(), self.getAltura())))
             elif self.opcao == 4:
+                print("EXERCÍCIO 4")
                 print("Digite sua idade:")
                 self.setIdade(int(input()))
                 print("Digite seus meses:")
@@ -92,6 +111,76 @@ class Control:
                 print("Digite seus dias:")
                 self.setDias(int(input()))
                 print("Sua idade em dias é: {}".format(self.model.exercicio04(self.getIdade(), self.getMeses(), self.getDias())))
-
+            elif self.opcao == 5:
+                print("EXERCÍCIO 5")
+                print("Digite o total de eleitores")
+                eleitores = float(input())
+                print("Digite o total de votos brancos: ")
+                brancos = float(input())
+                print("Digite o total de votos nulos: ")
+                nulos = float(input())
+                print("Digite o total de votos validos: ")
+                validos = float(input())
+                print(self.model.exercicio05(eleitores, brancos, nulos, validos))
+            elif self.opcao == 6:
+                print("EXERCÍCIO 6")
+                print("Informe o salário:")
+                salario = float(input())
+                print("Informe o percentual de reajuste:")
+                reajuste = float(input())
+                print(self.model.exercicio06(salario, reajuste))
+            elif self.opcao == 7:
+                print("EXERCÍCIO 7")
+                print("Informe o custo de fabrica:")
+                custoFabrica = float(input())
+                print("Informe o percentual do distribuidor:")
+                percentDistribuidor = float(input())
+                print("Informe o percentual de impostos:")
+                percentImpostos = float(input())
+                print(self.model.exercicio07(custoFabrica, percentDistribuidor, percentImpostos))
+            elif self.opcao == 8:
+                print("EXERCÍCIO 8")
+                print("Digite a primeira nota:")
+                nota1 = float(input())
+                print("Digite a segunda nota:")
+                nota2 = float(input())
+                print("Digite a terceira nota:")
+                nota3 = float(input())
+                print("A média é: {}".format(self.model.exercicio08(nota1, nota2, nota3)))
+            elif self.opcao == 9:
+                print("EXERCÍCIO 9")
+                print("Digite o total de maças compradas:")
+                self.setMacas(float(input()))
+                print("o total a pagar é: {}".format(self.model.exercicio09(self.getMacas())))
+            elif self.opcao == 10:
+                pass
+            elif self.opcao == 11:
+                print("EXERCÍCIO 11")
+                print("Informe o salário fixo do funcionário:")
+                salario = float(input())
+                print("Informe o número de vendas:")
+                vendas = float(input())
+                print(self.model.exercicio11(salario, vendas))
+            elif self.opcao == 12:
+                print("EXERCÍCIO 12")
+                print("Informe o número da sua conta:")
+                conta = int(input())
+                print("Informe o saldo atual:")
+                saldo = float(input())
+                print("Informe o valor de débito :")
+                debito = float(input())
+                print("Informe o valor de crédito :")
+                credito = float(input())
+                print(self.model.exercicio12(conta, saldo, debito, credito))
+            elif self.opcao == 13:
+                print("EXERCICIO 13")
+                print("Digite um número:")
+                num = int(input())
+                print(self.model.exercicio13(num))
+            elif self.opcao == 14:
+                print("EXERCÍCIO 14")
+                print("Digite um número:")
+                n = int(input())
+                print(self.model.exercicio14(n))
             else:
                 print("Opção inválida!")
--- ExerciciosPython/Model.py
+++ ExerciciosPython/Model.py
@@ -36,5 +36,64 @@ class Model:
         return base * altura
 
     def exercicio04(self, idade, meses, dias):
-        return idade * 365 + meses * 30 + dias
+        return (idade * 365) + (meses * 30) + dias
 
+    def exercicio05(self, eleitores, brancos, nulos, validos):
+        totalVotos = brancos + nulos + validos
+        if totalVotos == eleitores:
+            return "Total de eleitores: {}\nPercentual brancos: {}%\nPercentual nulos: {}%\nPercentual validos: {}%".format(eleitores, (eleitores * brancos) /100, (eleitores * nulos) / 100, (eleitores * validos) / 100)
+        else:
+            return "Quantidade de votos não bate com total de eleitores. Tente novamente."
+
+    def exercicio06(self, salario, reajuste):
+        salarioNovo = salario + (salario * reajuste) / 100
+        return "Seu salário reajustado é: R${}".format(salarioNovo)
+
+    def exercicio07(self, custoFabrica, percentualDistribuidor, percentualImposto):
+        totalImposto = percentualDistribuidor / 100 + percentualImposto / 100
+        custoFinal = custoFabrica + (custoFabrica + totalImposto)
+        return "O custo final ao consumidor é: R${}".format(custoFinal)
+
+    def exercicio08(self, nota1, nota2, nota3):
+        return (nota1 + nota2 + nota3) / 3
+
+    def exercicio09(self, macas):
+        if macas < 12:
+            return macas * 1.30
+        else:
+            return macas * 1.00
+
+    def exercicio10(self):
+        pass
+
+    def exercicio11(self, salario, vendas):
+        if vendas < 1:
+            return("Impossível calcular com vendas negativas.")
+        if vendas <= 1500.00:
+            totalVendas = (vendas * 3) /100
+            novoSalario = totalVendas + salario
+            return novoSalario
+        else:
+            vendasExtras = vendas - 1500.00
+            vendasExtras = (vendasExtras * 5) / 100
+            totalVendas = (vendas * 3) / 100
+            novoSalario = totalVendas + vendasExtras + salario
+            return novoSalario
+    def exercicio12(self, conta, saldo, debito, credito):
+        saldoAtual = saldo - debito + credito
+        if saldoAtual < 0:
+            return("Saldo atual é: R${}\nSaldo é negativo.".format(saldoAtual))
+        else:
+            return("Saldo atual é: R${}\nSaldo é positivo.".format(saldoAtual))
+
+    def exercicio13(self, num):
+        if (num > 0) & (num < 11):
+            msg = ""
+            for i in range(11):
+                msg = msg + "\n{} * {} = {}".format(num, i, num * i)
+            return msg
+        else:
+            return "Número inválido."
+
+    def exercicio14(self, n):
+        pass
